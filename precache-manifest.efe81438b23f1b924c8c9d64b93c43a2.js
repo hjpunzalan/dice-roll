e16c90b994137b6f78f4ad5ba794328f90cdccf5
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-05-12 19:09:15 +0800

    Updates

---
+++ precache-manifest.efe81438b23f1b924c8c9d64b93c43a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5228396529b4d6b2b4f09ed6df8756c2",
+    "url": "/dice-roll/index.html"
+  },
+  {
+    "revision": "d7a881bc93bf6d7cb4e3",
+    "url": "/dice-roll/static/css/main.4d050dc3.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/dice-roll/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "d7a881bc93bf6d7cb4e3",
+    "url": "/dice-roll/static/js/main.327c647a.chunk.js"
+  },
+  {
+    "revision": "c5e700a23ffa76a3a62a",
+    "url": "/dice-roll/static/js/runtime~main.bb3442c3.js"
+  }
+]);
