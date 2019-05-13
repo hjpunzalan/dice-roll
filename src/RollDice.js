385535a5c1a864fd266c32b9c9b5b0fb36a54e7b
Jonathan <hj.punzalan@hotmail.com>
2019-05-13 09:21:04 +0800

    change animation and made flexbox spacearound

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react';
-import Die from './Die';
-import './RollDice.css';
+import React, { Component } from "react";
+import Die from "./Die";
+import "./RollDice.css";
 
 class RollDice extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            firstDie: 'one',
-            secondDie: 'two',
+            firstDie: "one",
+            secondDie: "two",
             loading: false
-        }
+        };
         this.randomNumber = this.randomNumber.bind(this);
         this.changeNumber = this.changeNumber.bind(this);
     }
 
     randomNumber() {
-        const numbers = ['one', 'two', 'three', 'four', 'five', 'six'];
-        const random = Math.floor(Math.random()*numbers.length);
-        console.log(random);
+        const numbers = ["one", "two", "three", "four", "five", "six"];
+        const random = Math.floor(Math.random() * numbers.length);
         return numbers[random];
     }
 
     changeNumber() {
-        
         this.addAnimation();
         setTimeout(() => {
             this.removeAnimation();
         }, 1000);
-        
+
         setTimeout(() => {
             const firstNumber = this.randomNumber();
-            this.setState({firstDie: firstNumber});
+            this.setState({ firstDie: firstNumber });
             const secondNumber = this.randomNumber();
             this.setState({ secondDie: secondNumber });
-            
-        }, 700);
+        }, 500);
     }
 
     addAnimation = () => {
-        document.querySelector(".RollDice-dice").classList.add('animated', 'shake');
-        document.querySelector(".RollDice-button").classList.add('loading');
+        document
+            .querySelector(".RollDice-dice")
+            .classList.add("animated", "bounceInDown");
+        document.querySelector(".RollDice-button").classList.add("loading");
         this.setState({ loading: true });
-
-    }
+    };
 
     removeAnimation = () => {
-        document.querySelector(".RollDice-dice").classList.remove('animated', 'shake');
-        document.querySelector(".RollDice-button").classList.remove('loading');
+        document
+            .querySelector(".RollDice-dice")
+            .classList.remove("animated", "bounceInDown");
+        document.querySelector(".RollDice-button").classList.remove("loading");
         this.setState({ loading: false });
-    }
+    };
 
     render() {
         return (
@@ -57,12 +57,15 @@ class RollDice extends Component {
                     <Die number={this.state.firstDie} />
                     <Die number={this.state.secondDie} />
                 </div>
-                {this.state.loading ? 
-                    <button className="RollDice-button">Rolling ....</button> :
-                    <button onClick={this.changeNumber} className="RollDice-button">Roll Dice!</button>
-                }
+                {this.state.loading ? (
+                    <button className="RollDice-button">Rolling ....</button>
+                ) : (
+                        <button onClick={this.changeNumber} className="RollDice-button">
+                            Roll Dice!
+          </button>
+                    )}
             </div>
-        )
+        );
     }
 }
 
